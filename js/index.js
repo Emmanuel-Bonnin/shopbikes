464cb7d753a32f234714e8f97abf3f5ecadd3893
Emmanuel Bonnin <emmanuelbonnin92@gmail.com>
2022-07-19 23:32:48 -0300

    bicicleta

--- js/index.js
+++ js/index.js
@@ -1,6 +1,61 @@
+
+
+
+alert("Bienvenidos a SHOPBIKES")
+
+nombre = prompt("hola como te llamas ")
+alert(nombre + " selecciona una de las 3 marcas disponibles")
+
+
+
+
+
+
+
+function Bicicle() {
+    marcaBicicleta = prompt("puedes seleccionar gt , merida o scott");
+if ( marcaBicicleta == "gt") {
+    alert("Seleccionaste GT , el valor es de 600 usd")
+}
+else if ( marcaBicicleta == "scoot") {
+    alert("Seleccionaste SCOOT , el valor es de 900 usd")}
+
+
+else if ( marcaBicicleta == "merida") {
+        alert("Seleccionaste MERIDA , el valor es de 700 usd")
+
+}
+else {
+    alert("debes de selecionar una de las 3 opciones")
+}
+} 
+Bicicle()
+
+
+
+
+
+
+
+
+
+//navtoggle//
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 
 navToggle.addEventListener("click", ()=>{
     navMenu.classList.toggle("nav-menu_visible")
 })
+
+//alert("Bienvenidos a SHOPBIKES")
+
+
+
+
+
+
+
+
+
+
+
